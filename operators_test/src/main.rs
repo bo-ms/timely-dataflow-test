600be3dc922412dccaec5bceaa6d63783655da2c
Bo Chen <boche@microsoft.com>
2024-11-21 17:27:27 +0800

    Add accumulate operator

--- operators_test/src/main.rs
+++ operators_test/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     concatenate();
     aggregate();
+    accumulate();
     join();
 }
 
@@ -89,6 +90,63 @@ fn aggregate() {
                     |key| *key as u64,
                 )
                 .inspect(|x| println!("aggregated result: {:?}", x));
+        });
+ 
+        let input1_data: Vec<(u32, String)> = vec![(1, String::from("M1")), (2, String::from("M4"))];
+        let input2_data: Vec<(u32, String)> = vec![(1, String::from("M2")), (2, String::from("M5"))];
+        let input3_data: Vec<(u32, String)> = vec![(1, String::from("M3")), (3, String::from("M6"))];
+ 
+        // introduce input, advance computation
+        for round in 0..2 {
+            input1.send(input1_data[round].clone());
+            input2.send(input2_data[round].clone());
+            input3.send(input3_data[round].clone());
+ 
+            input1.advance_to(round + 1);
+            input2.advance_to(round + 1);
+            input3.advance_to(round + 1);
+ 
+            worker.step();
+        }
+    })
+    .unwrap();
+}
+
+fn accumulate() {
+
+    // https://docs.rs/timely/latest/timely/dataflow/operators/count/trait.Accumulate.html#required-methods
+
+    use timely::dataflow::operators::Accumulate;
+    use timely::dataflow::operators::input::Handle;
+    use timely::dataflow::operators::{Concatenate, Input, Inspect};
+    use timely::*;
+    print!("\n\nIn function accumulate\n");
+ 
+    timely::execute(Config::thread(), |worker| {
+        // add an input and base computation off of it
+        let mut input1: Handle<usize, (u32, String)> = Handle::new();
+        let mut input2 = Handle::new();
+        let mut input3 = Handle::new();
+        worker.dataflow(|scope| {
+            let stream1 = scope.input_from(&mut input1);
+            let stream2 = scope.input_from(&mut input2);
+            let stream3 = scope.input_from(&mut input3);
+ 
+            stream1.inspect(|x| println!("stream1: {:?}", x));
+            stream2.inspect(|x| println!("stream2: {:?}", x));
+            stream3.inspect(|x| println!("stream3: {:?}", x));
+ 
+            let concatenated_stream = scope
+                .concatenate(vec![stream1, stream2, stream3])
+                .inspect(|x| println!("concatenated stream:{:?}", x));
+
+            concatenated_stream
+                .accumulate(Vec::new(), |buffer, data| {
+                    for data in data.iter() {
+                        buffer.push(data.clone());
+                    }
+                })
+                .inspect(|x| println!("accumulated result: {:?}", x));
         });
  
         let input1_data: Vec<(u32, String)> = vec![(1, String::from("M1")), (2, String::from("M4"))];
